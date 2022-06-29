b338b61c0806eb095a56e66895c46631dae6b3c5
RafaelAbe0 <98974573+RafaelAbe0@users.noreply.github.com>
2022-06-29 20:55:52 +0000

    Add files via upload

---
+++ Blocos-Organizado.js
@@ -0,0 +1,240 @@
+[
+  Nivel 1-
+  { "id": 31, "texto": "bloco 1", "ordem": 1, "tipo": "tematica", "blocos_texto_nivel_2": 
+    [ 
+      Nivel 2-
+      { "id": 32, "texto": "sub bloco 1_1", "ordem": 1, "tipo": "artigo", "blocos_texto_nivel_3": 
+        [
+          Nivel 3-
+          { "id": 33, "texto": "sub bloco 1_1_1", "ordem": 1, "tipo": "paragrafo" 
+          } 
+        ] 
+      },
+      Nivel 2-
+      { "id": 34, "texto": "sub bloco 1_2", "ordem": 2, "tipo": "artigo" 
+      } 
+    ] 
+  },
+  Nivel 1-
+  { "id": 35, "texto": "bloco 2", "ordem": 2, "tipo": "tematica", "blocos_texto_nivel_2": 
+    [ 
+      { "id": 36, "texto": "sub bloco 2_1", "ordem": 1, "tipo": "paragrafo" 
+      } 
+    ] 
+  } 
+]
+--
+[
+  Nivel 1-
+  { "id": 147, "texto": "Uma epígrafe aqui", "ordem": 1, "tipo": "epigrafe" 
+  }, 
+  { "id": 148, "texto": "Uma ementa aqui", "ordem": 2, "tipo": "ementa" 
+  }, 
+  { "id": 149, "texto": "Um preâmbulo aqui", "ordem": 3, "tipo": "preambulo" 
+  }, 
+  { "id": 150, "texto": "Artigo antes da estrutura Parte #1", "ordem": 4, "tipo": "artigo" 
+  }, 
+  { "id": 151, "texto": "Artigo antes da estrutura Parte #2", "ordem": 5, "tipo": "artigo" 
+  }, 
+  { "id": 152, "texto": "Estrutura do tipo Parte #1", "ordem": 6, "tipo": "parte", "blocos_texto_nivel_2": 
+    [
+      Nivel 2-
+      { "id": 153, "texto": "Exemplo de Temática logo depois de uma estrutura PARTE", "ordem": 1, "tipo": "tematica", "blocos_texto_nivel_3": 
+        [
+          Nivel 3-
+          { "id": 154, "texto": "Um artigo aqui", "ordem": 1, "tipo": "artigo" 
+          } 
+        ] 
+      } 
+    ] 
+  }, 
+  { "id": 155, "texto": "Estrutura do tipo Parte #2 alt", "ordem": 7, "tipo": "parte", "blocos_texto_nivel_2": 
+    [ 
+      Nivel 2-
+      { "id": 156, "texto": "Um artigo aqui #1", "ordem": 1, "tipo": "artigo" 
+      }, 
+      { "id": 157, "texto": "Um artigo aqui #2 alt", "ordem": 2, "tipo": "artigo" 
+      }, 
+      { "id": 158, "texto": "Estrutura do tipo Livro #1", "ordem": 3, "tipo": "livro" 
+      }, 
+      { "id": 159, "texto": "Estrutura do tipo Livro #2", "ordem": 4, "tipo": "livro", "blocos_texto_nivel_3": 
+        [ 
+          Nivel 3-
+          { "id": 160, "texto": "Um artigo aqui #1", "ordem": 1, "tipo": "artigo" 
+          }, 
+          { "id": 161, "texto": "Um artigo aqui #2", "ordem": 2, "tipo": "artigo" 
+          }, 
+          { "id": 162, "texto": "Estrutura do tipo Título #1", "ordem": 3, "tipo": "titulo" 
+          }, 
+          { "id": 163, "texto": "Estrutura do tipo Título #2", "ordem": 4, "tipo": "titulo", "blocos_texto_nivel_4": 
+            [ 
+              Nivel 4-
+              { "id": 164, "texto": "Um artigo aqui #1", "ordem": 1, "tipo": "artigo" 
+              }, 
+              { "id": 165, "texto": "Um artigo aqui #2", "ordem": 2, "tipo": "artigo" 
+              }, 
+              { "id": 166, "texto": "Estrutura do tipo Capítulo #1", "ordem": 3, "tipo": "capitulo" 
+              }, 
+              { "id": 167, "texto": "Estrutura do tipo Capítulo #2", "ordem": 4, "tipo": "capitulo", "blocos_texto_nivel_5": 
+                [ 
+                  Nivel 5-
+                  { "id": 168, "texto": "Um artigo aqui #1", "ordem": 1, "tipo": "artigo" 
+                  }, 
+                  { "id": 169, "texto": "Um artigo aqui #2 realterado", "ordem": 2, "tipo": "artigo" 
+                  }, 
+                  { "id": 170, "texto": "Estrutura do tipo Seção #1", "ordem": 3, "tipo": "secao" 
+                  }, 
+                  { "id": 171, "texto": "Estrutura do tipo Seção #2", "ordem": 4, "tipo": "secao", "blocos_texto_nivel_6": 
+                    [ 
+                      Nivel 6-
+                      { "id": 172, "texto": "Um artigo aqui #1", "ordem": 1, "tipo": "artigo" 
+                      }, 
+                      { "id": 173, "texto": "Um artigo aqui #2", "ordem": 2, "tipo": "artigo" 
+                      }, 
+                      { "id": 174, "texto": "Estrutura do tipo Subseção #1", "ordem": 3, "tipo": "subsecao" 
+                      }, 
+                      { "id": 175, "texto": "Estrutura do tipo Subseção #2", "ordem": 4, "tipo": "subsecao", "blocos_texto_nivel_7": 
+                        [ 
+                          Nivel 7-
+                          { "id": 176, "texto": "Um artigo aqui #1", "ordem": 1, "tipo": "artigo" 
+                          }, 
+                          { "id": 177, "texto": "Um artigo aqui #2", "ordem": 2, "tipo": "artigo" 
+                          }, 
+                          { "id": 178, "texto": "Estrutura do tipo Temática #1", "ordem": 3, "tipo": "tematica" 
+                          }, 
+                          { "id": 179, "texto": "Estrutura do tipo Temática #2", "ordem": 4, "tipo": "tematica", "blocos_texto_nivel_8": 
+                            [ 
+                              Nivel 8-
+                              { "id": 180, "texto": "Um artigo aqui #1", "ordem": 1, "tipo": "artigo", "blocos_texto_nivel_9": 
+                                [ 
+                                  Nivel 9-
+                                  { "id": 181, "texto": "Um parágrafo aqui #1", "ordem": 1, "tipo": "paragrafo" 
+                                  }, 
+                                  { "id": 182, "texto": "Um parágrafo aqui #2", "ordem": 2, "tipo": "paragrafo", "blocos_texto_nivel_10": 
+                                    [ 
+                                      Nivel 10-
+                                      { "id": 183, "texto": "Um inciso aqui #1", "ordem": 1, "tipo": "inciso" 
+                                      }, 
+                                      { "id": 184, "texto": "Um inciso aqui #2", "ordem": 2, "tipo": "inciso" 
+                                      } 
+                                    ] 
+                                  } 
+                                ] 
+                              },
+                              Nivel 8- 
+                              { "id": 185, "texto": "Um artigo aqui #2", "ordem": 2, "tipo": "artigo", "blocos_texto_nivel_9": 
+                                [ 
+                                  Nivel 9-
+                                  { "id": 186, "texto": "Um inciso aqui #1", "ordem": 1, "tipo": "inciso" 
+                                  }, 
+                                  { "id": 187, "texto": "Um inciso aqui #2", "ordem": 2, "tipo": "inciso", "blocos_texto_nivel_10": 
+                                    [ 
+                                      Nivel 10-
+                                      { "id": 188, "texto": "Uma alínea aqui #1", "ordem": 1, "tipo": "alinea", "blocos_texto_nivel_11": 
+                                        [ 
+                                          Nivel 11-
+                                          { "id": 189, "texto": "Um item aqui #1", "ordem": 1, "tipo": "item" 
+                                          }, 
+                                          { "id": 190, "texto": "Um item aqui #2", "ordem": 2, "tipo": "item" 
+                                          } 
+                                        ] 
+                                      },
+                                      Nivel 10- 
+                                      { "id": 191, "texto": "Uma alínea aqui #2", "ordem": 2, "tipo": "alinea", "blocos_texto_nivel_11": 
+                                        [ 
+                                          Nivel 11-
+                                          { "id": 192, "texto": "Um item aqui #1", "ordem": 1, "tipo": "item" 
+                                          }, 
+                                          { "id": 193, "texto": "Um item aqui #2", "ordem": 2, "tipo": "item" 
+                                          } 
+                                        ] 
+                                      } 
+                                    ] 
+                                   } 
+                                ] 
+                              },
+                              Nivel 8- 
+                              { "id": 194, "texto": "Um artigo aqui #3", "ordem": 3, "tipo": "artigo", "blocos_texto_nivel_9": 
+                                [ 
+                                  Nivel 9-
+                                  { "id": 195, "texto": "Um inciso aqui #1", "ordem": 1, "tipo": "inciso" 
+                                  }, 
+                                  { "id": 196, "texto": "Um inciso aqui #2", "ordem": 2, "tipo": "inciso" 
+                                  }, 
+                                  { "id": 197, "texto": "Um parágrafo aqui #1", "ordem": 3, "tipo": "paragrafo" 
+                                  }, 
+                                  { "id": 198, "texto": "Um parágrafo aqui #2", "ordem": 4, "tipo": "paragrafo" 
+                                  } 
+                                ] 
+                              } 
+                            ] 
+                          } 
+                        ] 
+                      } 
+                    ] 
+                  },
+                  Nivel 5- 
+                  { "id": 202, "texto": "Um artigo aqui #3", "ordem": 5, "tipo": "artigo" 
+                  }, 
+                  { "id": 203, "texto": "Estrutura do tipo Seção #3", "ordem": 6, "tipo": "secao" 
+                  } 
+                ] 
+              } 
+            ] 
+          } 
+        ] 
+      } 
+    ] 
+  },
+  Nivel 1- 
+  { "id": 199, "texto": "Goiânia, 16 de julho de 2020, 132º da República.", "ordem": 8, "tipo": "localdata" 
+  }, 
+  { "id": 200, "texto": "RONALDO CAIADO\\nGovernador do Estado\\n(D.O. de 17-07-2020)", "ordem": 9, "tipo": "assinaturaautores" 
+  }, 
+  { "id": 201, "texto": "Este texto não substitui o publicado no D.O. de 17-07-2020.", "ordem": 10, "tipo": "avisolegal" 
+  } 
+]
+--
+[ 
+  Nivel 1-
+  { "id": 43, "texto": "bloco 1", "ordem": 1, "tipo": "tematica", "blocos_texto_nivel_2": 
+    [ 
+      Nivel 2-
+      { "id": 44, "texto": "sub bloco 1_1", "ordem": 1, "tipo": "artigo", "blocos_texto_nivel_3": 
+        [ 
+          Nivel 3-
+          { "id": 45, "texto": "sub bloco 1_1_1", "ordem": 1, "tipo": "paragrafo" 
+          } 
+        ] 
+      },
+      Nivel 2-
+      { "id": 46, "texto": "sub bloco 1_2", "ordem": 2, "tipo": "artigo" 
+      } 
+    ] 
+  },
+  Nivel 1- 
+  { "id": 47, "texto": "bloco 2", "ordem": 2, "tipo": "tematica", "blocos_texto_nivel_2": 
+  [ 
+    Nivel 2-
+    { "id": 48, "texto": "sub bloco 2_1", "ordem": 1, "tipo": "paragrafo" 
+    } 
+  ] 
+  } 
+]
+use:{
+  id: bloco.id;
+  tipo: bloco.tipo
+  texto: bloco.texto;
+  count++;
+}
+for(int i=1, count=1; i<=11; i++){
+  bloco.nivel_'i';{
+    use;
+    if(bloco.nivel_'count' exits?){
+      bloco.nivel_'count';{
+        use;
+        repeat if;
+      }
+    }
+  }
+}
